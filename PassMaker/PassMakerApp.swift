a2165ccc6c814cdec7e6a3d6bb8824a5fbaefc8a
Paul van Woensel <pvanwoensel@gmail.com>
2023-02-18 10:16:27 -0600

    Initial Commit

---
+++ PassMaker/PassMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassMakerApp.swift
+//  PassMaker
+//
+//  Created by Paul van Woensel on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PassMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
